db793699c0bcb4bd8f47eb147330c5903a049edb
Alexandder29 <blabla_xandder@yahoo.com>
2022-04-07 22:19:09 +0300

    Added states and behaviors, work in progress

---
+++ src/ro/fasttrackit/lab7/Objects/ApplicationBottle.java
@@ -0,0 +1,11 @@
+package ro.fasttrackit.lab7.Objects;
+
+public class ApplicationBottle {
+    public static void main(String[] args) {
+        Bottle first = new Bottle(1000, 750, true);
+        Bottle second = new Bottle(500, 0, false);
+        Bottle third = new Bottle(750, 250, false);
+    }
+
+
+}
--- src/ro/fasttrackit/lab7/Objects/Bottle.java
+++ src/ro/fasttrackit/lab7/Objects/Bottle.java
@@ -1,5 +1,26 @@
 package ro.fasttrackit.lab7.Objects;
 
 public class Bottle {
+    private int totalCapacity;
+    private int availableLiquid;
+    private boolean open;
+
+    Bottle(int totalCapacity, int availableLiquid, boolean open) {
+        this.totalCapacity = totalCapacity;
+        this.availableLiquid = availableLiquid;
+        this.open = open;
+    }
+
+    public int getTotalCapacity() {
+        return totalCapacity;
+    }
+
+    public int getAvailableLiquid() {
+        return availableLiquid;
+    }
+
+    public boolean getOpen() {
+        return open;
+    }
 
 }
