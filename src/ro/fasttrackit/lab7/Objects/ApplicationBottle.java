72867e59169ba3dc8a78121a664b219eff848fc7
Alexandder29 <blabla_xandder@yahoo.com>
2022-04-09 20:40:19 +0300

    App bottle (hw lab7) finished

--- src/ro/fasttrackit/lab7/Objects/ApplicationBottle.java
+++ src/ro/fasttrackit/lab7/Objects/ApplicationBottle.java
@@ -5,7 +5,19 @@ public class ApplicationBottle {
         Bottle first = new Bottle(1000, 750, true);
         Bottle second = new Bottle(500, 0, false);
         Bottle third = new Bottle(750, 250, false);
+
+        System.out.println("First bottle has " + first.getTotalCapacity() + " ml and there's " + first.getAvailableLiquid() + " ml liquid left.");
+        System.out.println(first.toDrink(300));
+
+        System.out.println("Second bottle has " + second.getTotalCapacity() + " ml and there's " + second.getAvailableLiquid() + " ml liquid left.");
+        System.out.println(second.toDrink(100));
+        second.open();
+        System.out.println(second.toDrink(150));
+        second.close();
+
+        System.out.println("Third bottle has " + third.getTotalCapacity() + " ml and there's " + third.getAvailableLiquid() + " ml liquid left.");
+        third.open();
+        System.out.println(third.toDrink(50));
+        third.close();
     }
-
-
 }
--- src/ro/fasttrackit/lab7/Objects/Bottle.java
+++ src/ro/fasttrackit/lab7/Objects/Bottle.java
@@ -23,4 +23,37 @@ public class Bottle {
         return open;
     }
 
+    public boolean getClose() {
+        return !open;
+    }
+
+    public void open() {
+        if (!getOpen()) {
+            this.open = true;
+            System.out.println("You opened the bottle.");
+        } else {
+            System.out.println("Bottle already opened.");
+        }
+    }
+
+    public void close() {
+        if (getClose()) {
+            System.out.println("Bottle already closed.");
+        } else {
+            this.open = false;
+            System.out.println("You closed the bottle.");
+        }
+    }
+
+    public String toDrink(int howMuch) {
+        if (getClose()) {
+            return "First you need to open the bottle";
+        }
+
+        if (availableLiquid < howMuch) {
+            return "There's not enough liquid.";
+        }
+        availableLiquid = availableLiquid - howMuch;
+        return "You've dranked " + howMuch + " liquid. There's " + availableLiquid + " left.";
+    }
 }
